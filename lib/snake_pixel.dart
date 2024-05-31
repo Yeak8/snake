44ab31f9a3c8e7361eb3c9980fd42db86eac51a2
yeak8 <rgrajalescastillo@gmail.com>
2024-05-30 23:07:58 -0600

    Initial Commit

--- lib/snake_pixel.dart
+++ lib/snake_pixel.dart
@@ -1,1 +1,18 @@
+import 'package:flutter/material.dart';
 
+class SnakePixel extends StatelessWidget {
+  const SnakePixel({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(3.0),
+      child: Container(
+        decoration: BoxDecoration(
+          color: Colors.red,
+          borderRadius: BorderRadius.circular(4),
+        ),
+      ),
+    );
+  }
+}
