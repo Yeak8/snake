0542e566278a936f92d90a38d46eb3733324a014
yeak8 <rgrajalescastillo@gmail.com>
2024-05-31 19:18:11 -0600

    Initial Commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,22 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'home_page.dart';
 
 Future main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+
+  Platform.isAndroid
+      ? await Firebase.initializeApp(
+          options: FirebaseOptions(
+          apiKey: "AIzaSyB5vYd_0ZzpSodEgHaGFfYP0R4508w6qWg",
+          appId: "1:252698512656:android:d75615429bf2e21dcbe995",
+          messagingSenderId: "252698512656",
+          projectId: "snake-b26e5",
+        ))
+      : await Firebase.initializeApp();
+
   runApp(const MainApp());
 }
 
